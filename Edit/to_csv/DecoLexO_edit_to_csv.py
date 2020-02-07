f861ca4ff74f4f8bf54fd07681572d34d1bb1850
GoDK36 <siinwoo0306@gmail.com>
2020-02-07 14:59:54 +0900

    header 값 설정, 인코딩 값 utf-8-sig 설정

--- Edit/to_csv/DecoLexO_edit_to_csv.py
+++ Edit/to_csv/DecoLexO_edit_to_csv.py
@@ -1,3 +1,3 @@
 ##csv파일로 만들기
 def df2csv(df, filepath):
-    df.to_csv(filepath, header=False, index=False, na_rep='')
+    df.to_csv(filepath, header=True, index=False, na_rep='', encoding='utf-8-sig')
